d2c78e5726758fc25c6cf19d634ae60ff34dedb0
name-al <proganteng1508@gmail.com>
2026-08-15 16:59:25 +0700

    initial commit

---
+++ FirstTime.js
@@ -0,0 +1,64 @@
+// var topup = 50;
+// var frekuensi = 3;
+// var diskon = 2;
+
+// var totalTopup = topup * frekuensi;
+// var totalDiskon = totalTopup - diskon * frekuensi  ;
+// console.log("Total Diskon: " + totalDiskon);
+
+// var curScore = 1858;
+// var exp = 50;
+// var scoreExp = 500;
+// var score = (scoreExp / exp * 200 ) - curScore 
+// console.log("Score: " + score);
+
+// var a = 15; 
+// var b = 23;
+// var c = 228; 
+// var d = 500;
+// var e = 100;
+
+// a += 2;
+// b -= 3;
+// c %= 5;
+// d *= 2;
+// e /= 25;
+// console.log("a: " + a);
+// console.log("b: " + b);
+// console.log("c: " + c);
+// console.log("d: " + d);
+// console.log("e: " + e);
+
+// var word1 = "Hello";
+// var word2 = "World";
+
+// var concat = word1+word2;
+// var subtract = word1-word2;
+
+// console.log(concat);
+// console.log(subtract);
+
+// var score = 1500;
+// var displayScore = "Score : "+ score;
+// console.log(displayScore);
+
+// var name = "John";
+// var age = 30;
+// var target = "Programmer";
+// console.log("My name is " + name + ", I am " + age + " years old and I want to be a " + target + ".");
+// var Belanjaan = ["Roti gandum ", "Pasta ", "Saus Tomat ", "Lowfat yogurt ", "Butter"];
+// console.log(Belanjaan);
+// var length = Belanjaan.length;
+// console.log("Jumlah belanjaan: " + length);
+// var importantItem = Belanjaan[0];
+// console.log("Item penting: " + importantItem);
+// Belanjaan[3] = 'soya milk'; 
+// console.log(Belanjaan);
+// var length = Belanjaan.length;
+// console.log("Jumlah belanjaan: " + length);
+
+var holidayPlan = ["paris", "tokyo", "new york", "sydney"];
+console.log(holidayPlan);
+holidayPlan[4] = 'bali';
+holidayPlan[5] = 'hokkaido';
+console.log(holidayPlan);
